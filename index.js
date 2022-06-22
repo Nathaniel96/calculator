cb557da3e2534d8e3055cfd90417211293aadaac
lawnathan <97894759+lawnathan@users.noreply.github.com>
2022-06-22 13:54:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var a,b,Addition, Subtraction, Multiplication, Division;
+a=parseInt(prompt("enter first number" ));
+b=parseInt(prompt("enter second number"));
+Addition = a + b;
+Subtraction= a-b;
+Multiplication= a*b;
+Division=a/b
+document.write("Addittion =" + Addition + "<br> " + "subtraction = " + Subtraction + 
+"<br>" + "Multiplication =" + Multiplication + "<br>" + "Division =" + Division)
